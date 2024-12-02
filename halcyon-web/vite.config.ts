9975f1a54a9db6d0843d18bfc9f2ec06d7711672
Chris Poulter <cpoulter@hotmail.co.uk>
2024-12-02 11:53:04 +0000

    set vite port

--- halcyon-web/vite.config.ts
+++ halcyon-web/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
+import { env } from 'process';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
     server: {
-        port: 59180,
+        port: env.PORT ? parseInt(env.PORT) : undefined,
     }
 })
