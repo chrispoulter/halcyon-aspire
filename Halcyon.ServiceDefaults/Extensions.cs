1117fcd02809a2e11754183e578ea762f5a3c7c5
Chris Poulter <cpoulter@hotmail.co.uk>
2024-12-12 13:52:56 +0000

    add healthcheck ui

--- Halcyon.ServiceDefaults/Extensions.cs
+++ Halcyon.ServiceDefaults/Extensions.cs
@@ -1,3 +1,4 @@
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.DependencyInjection;
@@ -114,12 +115,19 @@ public static class Extensions
         //if (app.Environment.IsDevelopment())
         //{
         // All health checks must pass for app to be considered ready to accept traffic after starting
-        app.MapHealthChecks("/health");
+        app.MapHealthChecks(
+            "/health",
+            new HealthCheckOptions { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse }
+        );
 
         // Only health checks tagged with the "live" tag must pass for app to be considered alive
         app.MapHealthChecks(
             "/alive",
-            new HealthCheckOptions { Predicate = r => r.Tags.Contains("live") }
+            new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+                Predicate = r => r.Tags.Contains("live"),
+            }
         );
         //}
 
