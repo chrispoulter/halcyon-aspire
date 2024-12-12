adc8a602cc982b2b95791d470cc3a0e9e9b65ebb
Chris Poulter <cpoulter@hotmail.co.uk>
2024-12-12 11:47:40 +0000

    use dynamic port in dockerfile

--- Halcyon.AppHost/Program.cs
+++ Halcyon.AppHost/Program.cs
@@ -44,7 +44,7 @@ var api = builder
 var web = builder
     .AddNpmApp("web", "../halcyon-web", scriptName: "dev")
     .WithEnvironment("VITE_API_URL", api.GetEndpoint("https"))
-    .WithHttpEndpoint(port: 5173, targetPort: 80, env: "PORT")
+    .WithHttpEndpoint(port: 5173, env: "PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
 
