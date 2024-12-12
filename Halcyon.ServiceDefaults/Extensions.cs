5cf8ab8b5e87a6f0a2c6d0ef9e5a9c5c45426a72
Chris Poulter <cpoulter@hotmail.co.uk>
2024-12-12 14:43:58 +0000

    remove debug code and update builds

--- Halcyon.ServiceDefaults/Extensions.cs
+++ Halcyon.ServiceDefaults/Extensions.cs
@@ -1,4 +1,3 @@
-using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.DependencyInjection;
@@ -115,19 +114,12 @@ public static class Extensions
         //if (app.Environment.IsDevelopment())
         //{
         // All health checks must pass for app to be considered ready to accept traffic after starting
-        app.MapHealthChecks(
-            "/health",
-            new HealthCheckOptions { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse }
-        );
+        app.MapHealthChecks("/health");
 
         // Only health checks tagged with the "live" tag must pass for app to be considered alive
         app.MapHealthChecks(
             "/alive",
-            new HealthCheckOptions
-            {
-                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
-                Predicate = r => r.Tags.Contains("live"),
-            }
+            new HealthCheckOptions { Predicate = r => r.Tags.Contains("live") }
         );
         //}
 
