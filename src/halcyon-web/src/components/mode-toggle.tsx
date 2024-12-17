2fca541513615de5cb4e7eb5a4cc3a0be7169890
Chris Poulter <cpoulter@hotmail.co.uk>
2024-12-17 12:51:01 +0000

    add aria labels to mode toggle

--- src/halcyon-web/src/components/mode-toggle.tsx
+++ src/halcyon-web/src/components/mode-toggle.tsx
@@ -12,13 +12,18 @@ export function ModeToggle({ className }: ModeToggleProps) {
     return (
         <button
             type="button"
+            aria-label="Toggle dark mode"
             className={cn(
                 'rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700',
                 className
             )}
             onClick={toggleTheme}
         >
-            {theme === 'dark' ? <Sun /> : <Moon />}
+            {theme === 'dark' ? (
+                <Sun aria-label="Currently light mode" />
+            ) : (
+                <Moon aria-label="Currently dark mode" />
+            )}
         </button>
     );
 }
