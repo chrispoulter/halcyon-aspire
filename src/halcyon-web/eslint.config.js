f2fd55c83abc54f07df87a0cd51b6c3743dd029c
Chris Poulter <cpoulter@hotmail.co.uk>
2024-12-18 10:16:07 +0000

    roll back lint-staged for now

--- src/halcyon-web/eslint.config.js
+++ src/halcyon-web/eslint.config.js
@@ -21,7 +21,7 @@ export default tseslint.config(
         rules: {
             ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': [
-                'error',
+                'warn',
                 { allowConstantExport: true }
             ]
         }
