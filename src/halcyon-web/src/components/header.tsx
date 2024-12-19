e752d3519edeb4dbde9b5ae0b65cfd50eed6a48c
Chris Poulter <cpoulter@hotmail.co.uk>
2024-12-19 15:43:52 +0000

    add badge

--- src/halcyon-web/src/components/header.tsx
+++ src/halcyon-web/src/components/header.tsx
@@ -1,16 +1,19 @@
 'use client';
 
 import Link from 'next/link';
+import { LogOut, User } from 'lucide-react';
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
-import { ModeToggle } from '@/components/mode-toggle';
+import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
+    DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { ModeToggle } from '@/components/mode-toggle';
 
 export function Header() {
     const onLogout = () => {
@@ -18,59 +21,77 @@ export function Header() {
     };
 
     return (
-        <header className="border-b">
-            <nav className="flex items-center justify-between gap-2 px-6 py-2 mx-auto max-w-screen-sm">
+        <header className="mx-auto flex max-w-screen-sm items-center justify-between gap-2 p-6">
+            <Link
+                href="/"
+                className="scroll-m-20 text-xl font-semibold tracking-tight"
+            >
+                Halcyon
+            </Link>
+
+            <nav className="flex items-center gap-2 leading-7">
                 <Link
                     href="/"
-                    className="scroll-m-20 text-2xl font-semibold tracking-tight"
+                    className="font-medium text-primary underline underline-offset-4"
                 >
-                    Halcyon
-                </Link>
-                <Link href="/" className="ml-auto text-sm">
                     Home
                 </Link>
-
-                <Link href="/user" className='text-sm'>Users</Link>
-
-                <Button asChild variant="secondary">
-                    <Link href="/account/register">Register</Link>
-                </Button>
-
-                <Button asChild variant="secondary">
-                    <Link href="/account/login">Login</Link>
-                </Button>
-
-                <ModeToggle />
-
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button
-                            variant="outline"
-                            className="h-auto w-auto rounded-full p-1"
-                        >
-                            <Avatar>
-                                <AvatarImage
-                                    src="https://github.com/shadcn.png"
-                                    alt="@shadcn"
-                                />
-                                <AvatarFallback>CN</AvatarFallback>
-                                <span className="sr-only">
-                                    Toggle profile menu
-                                </span>
-                            </Avatar>
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuItem asChild>
-                            <Link href="/profile">My Profile</Link>
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem onClick={onLogout}>
-                            Log out
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                <Link
+                    href="/user"
+                    className="font-medium text-primary underline underline-offset-4"
+                >
+                    Users
+                </Link>
             </nav>
+
+            <Button asChild variant="secondary" className="ml-auto">
+                <Link href="/account/register">Register</Link>
+            </Button>
+
+            <Button asChild variant="secondary">
+                <Link href="/account/login">Login</Link>
+            </Button>
+
+            <ModeToggle />
+
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Avatar>
+                        <AvatarImage
+                            src="https://github.com/shadcn.png"
+                            alt="System Administrator"
+                        />
+                        <AvatarFallback>CN</AvatarFallback>
+                        <span className="sr-only">Toggle profile menu</span>
+                    </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                    <DropdownMenuLabel className="flex flex-col gap-2">
+                        <span className="truncate">System Adminstrator</span>
+                        <span className="truncate text-sm text-muted-foreground">
+                            system.administrator@example.com
+                        </span>
+                        <Badge variant="secondary" className="justify-center">
+                            System Administrator
+                        </Badge>
+                        <Badge variant="secondary" className="justify-center">
+                            User Administrator
+                        </Badge>
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem asChild>
+                        <Link href="/profile">
+                            <User />
+                            <span>Profile</span>
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem onClick={onLogout}>
+                        <LogOut />
+                        <span>Log out</span>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </header>
     );
 }
