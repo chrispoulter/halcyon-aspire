32be86e154308574bc8d9d5dc0d9afab5b484872
Chris Poulter <cpoulter@hotmail.co.uk>
2025-02-17 11:58:08 +0000

    bugfix project name typo

--- src/Halcyon.AppHost/Program.cs
+++ src/Halcyon.AppHost/Program.cs
@@ -51,7 +51,7 @@ var api = builder
     .WaitFor(maildev);
 
 var web = builder
-    .AddNpmApp("web", "../Halyon.Web", scriptName: "dev")
+    .AddNpmApp("web", "../Halcyon.Web", scriptName: "dev")
     .WithEnvironment("VITE_API_URL", api.GetEndpoint("https"))
     .WithHttpEndpoint(port: 5173, env: "VITE_PORT", isProxied: false)
     .WithExternalHttpEndpoints()
